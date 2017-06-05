94dbd0ecbf3ee94033211295d041ab4b12f3be78
Ercan Baran <ercan.baran@smartface.io>
2017-06-05 09:46:43 +0000

    new pages added

---
+++ scripts/pages/tabs/profile/pgProfileDetail.js
@@ -0,0 +1,23 @@
+const extend = require('js-base/core/extend');
+const Page = require('nf-core/ui/page');
+const Color = require('nf-core/ui/color');
+
+const Page_ = extend(Page)(
+	// Constructor
+	function(_super){
+		// Initalizes super class for this page scope
+		_super(this, {
+			onLoad: onLoad.bind(this)
+		});
+});
+
+function onLoad() { 
+    this.headerBar.visible = true;
+    this.headerBar.title = "pgProfileDetail";
+    this.headerBar.titleColor = Color.create("#000000");
+    this.headerBar.backgroundColor = Color.create("#FFFFFF");
+    this.statusBar.visible = true;
+    this.statusBar.android && (this.statusBar.android.color = Color.create("#00A1F1"));
+}
+
+module && (module.exports = Page_);
