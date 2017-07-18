61e89b08425b1210a5fda6569f55697485dcc7ef
ErayTuncer <eray.tuncer@smartface.io>
2017-07-18 13:15:57 +0000

    add functionality to REQUEST button.

--- scripts/pages/tabs/hr/pgNewLeaveRequest.js
+++ scripts/pages/tabs/hr/pgNewLeaveRequest.js
@@ -2,6 +2,7 @@
 const extend = require('js-base/core/extend');
 const Color = require('sf-core/ui/color');
 const PageDesign = require("../../../ui/ui_pgNewLeaveRequest");
+const Router = require("sf-core/router");
 
 const Page_ = extend(PageDesign)(
 	// Constructor
@@ -20,6 +21,9 @@ function onShow(parentOnShow) {
 	this.headerBar.itemColor = Color.WHITE;
 	this.itemSpeech.page = this;
 	this.itemSpeech.inputField = this.textbox1;
+	this.btnRequest.onPress = function() {
+		Router.goBack("tabs/hr/leaveManagement");
+	};
 }
 
 function initTexts() {
