dc9a0489613b736ddf0293735e98b8dae0f008e7
ErayTuncer <register@eraytuncer.me>
2017-07-21 10:31:48 +0000

    use Speech2Text library and fix style problem.

--- scripts/components/ItemSpeech.js
+++ scripts/components/ItemSpeech.js
@@ -1,10 +1,8 @@
 const extend			= require('js-base/core/extend');
-const SpeechRecognizer	= require("sf-core/speechrecognizer");
 const System			= require("sf-core/device/system");
-const Application		= require("sf-core/application");
 const ItemSpeechDesign  = require('library/ItemSpeech');
 const Image             = require('sf-core/ui/image');
-const Timer             = require("sf-core/timer");
+const Speech2Text       = require("sf-extension-utils/speech2text");
 
 const ItemSpeech = extend(ItemSpeechDesign)(
 	//constructor
@@ -40,55 +38,21 @@ const ItemSpeech = extend(ItemSpeechDesign)(
 		
 		this.onTouchEnded = function()
 		{
-			invalidate(this);
+			startSpeechRecognizer(this);
 		}.bind(this);
 		
 	}
 	
 );
 
-function invalidate(item)
-{
-	if (!SpeechRecognizer.isRunning()) 
-		{
-		    item.inputField.text = "";
-            if (System.OS === "iOS") {
-                startSpeechRecognizer(item);
-            }
-            else if (System.OS === "Android") {
-                const RECORD_AUDIO_CODE = 1002;
-                Application.android.requestPermissions(RECORD_AUDIO_CODE, Application.android.Permissions.RECORD_AUDIO);
-                Application.android.onRequestPermissionsResult = function(e) {
-                    if (e.requestCode === RECORD_AUDIO_CODE && e.result) {
-                        startSpeechRecognizer(item);
-                    }
-                };
-            }
-        }
-        else {
-            Timer.clearAllTimer();
-            SpeechRecognizer.stop();
-            item.icon.alpha = 1;
-            item.icon.image = Image.createFromFile("images://icon_microphone.png");
-        }
-}
-
 function startSpeechRecognizer(item) 
 {
-    item.icon.image = Image.createFromFile("images://icon_microphone_red.png");
-    SpeechRecognizer.start({
-        onResult: function(result) {
-            item.inputField.text = result;
-        },
-        onFinish: function(result) {
-            Timer.clearAllTimer();
-            item.icon.image = Image.createFromFile("images://icon_microphone.png");
-        },
-        onError: function(error) {
-            Timer.clearAllTimer();
-            item.icon.image = Image.createFromFile("images://icon_microphone.png");
-        }
-    });
+	if (!Speech2Text.isRunning) {
+	    item.icon.image = Image.createFromFile("images://icon_microphone_red.png");
+		Speech2Text.startType(item.inputField, 3000, function() {
+		    item.icon.image = Image.createFromFile("images://icon_microphone.png");
+		});
+	}
 }
 
 module && (module.exports = ItemSpeech);
