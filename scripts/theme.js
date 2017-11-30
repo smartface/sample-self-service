55e3f7dc3d534b10a90afbbb27ddd2fa52a3c710
Ali Tuğrul PINAR <ali.pinar@smartface.io>
2017-11-30 06:16:33 +0000

    Fix getting currentTheme from Data

--- scripts/theme.js
+++ scripts/theme.js
@@ -1,13 +1,16 @@
 const Application = require("sf-core/application");
-
+const Data = require('sf-core/data');
 const config = require("./settings.json").config;
 const themeConfig = config.theme;
-
+const currentTheme = Data.getStringVariable("theme") || themeConfig.currentTheme;
 const createThemeContextBound = require("@smartface/contx/lib/styling/ThemeContext").createThemeContextBound;
 const themeSources = [];
 
+if (Data.getStringVariable("theme") == null)
+    Data.setStringVariable("theme", themeConfig.currentTheme);
+    
 themeConfig.themes.forEach(function(name){
-    themeSources.push({name: name, rawStyles: require("./themes/"+name), isDefault: themeConfig.currentTheme === name});
+    themeSources.push({name: name, rawStyles: require("./themes/"+name), isDefault: currentTheme === name});
 });
 
 Application.theme = createThemeContextBound(themeSources);
