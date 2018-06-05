d3f6d682402930609dce0b11b56dec6f4ae90b59
Dogan Ekici <doganekici26@gmail.com>
2018-06-05 13:34:50 +0000

    Updated newleaverequest page

--- scripts/pages/tabs/hr/pgNewLeaveRequest.js
+++ scripts/pages/tabs/hr/pgNewLeaveRequest.js
@@ -7,6 +7,8 @@ const Dialog = require('sf-core/ui/dialog');
 const FlexLayout = require('sf-core/ui/flexlayout');
 const Screen = require('sf-core/device/screen');
 const Calendar = require("@smartface/sf-component-calendar/components/Calendar");
+const Button = require('sf-core/ui/button');
+const Picker = require("sf-core/ui/picker");
 
 const Page_ = extend(PageDesign)(
 	// Constructor
@@ -30,28 +32,109 @@ function onShow(parentOnShow) {
 	this.itemSpeech.page = this;
 	this.itemSpeech.inputField = this.textbox1;
 	this.btnRequest.onPress = function() {
-		//Router.goBack("tabs/hr/leaveManagement");
+		Router.goBack();
 	};
 }
 
 function onLoad(parentOnLoad) {
 	if (typeof parentOnLoad === "function") parentOnLoad();
+	
+    var items = [
+        "Annual Leave",
+        "Medical Leave",
+        "Family Leave",
+        "Personal Leave",
+        "Maternity/Paternity Leave",
+        "Administrative Leave",
+        "Civic Duty",
+        "Other"
+    ];
+    var myPicker = new Picker({
+        items: items,
+        index: 0
+    });
+    var okCallback = function(e) {
+    	self.leaveTypePicker.pickerText.text = items[e.index];
+    }
+    var cancelCallback = function() {};
+        
+	var self = this;
     var myDialog = new Dialog();
-    myDialog.layout.alignItems = FlexLayout.AlignItems.CENTER;
-    myDialog.layout.justifyContent = FlexLayout.JustifyContent.CENTER;
+	myDialog.layout.backgroundColor = Color.create(70,0,0,0);
 	
 	var margin = 30;
 	const myCalendar = new Calendar();
 	myCalendar.left = margin
+	var height = Screen.width - margin*2;
 	myCalendar.width = Screen.width - margin*2;
-	myCalendar.height = Screen.width - margin*2;
-	myCalendar.top = (Screen.height - (Screen.width - margin*2))/2;
+	myCalendar.height = height;
+	var top = (Screen.height - (Screen.width - margin*2))/3;
+	myCalendar.top = top;
+	myCalendar.changeCalendar("en","gregorian",{});
+	var selectedDate;
+	myCalendar.onRangeSelectionComplete = function(startDate,endDate){
+	  selectedDate = startDate.date.day + "/" +  startDate.date.month + "/" + startDate.date.year + " - " + endDate.date.day + "/" +  endDate.date.month + "/" + endDate.date.year
+	}
+	const newMonthLabelColorStyle = {
+	  ".calendar":{
+	    ".header":{
+	      "&_navbar":{
+	        "&_monthLabel":{
+	          "textColor": "#1775d0"
+	        }
+	      }
+	    },
+	    ".day":{
+	      "&-selected":{
+	        "backgroundColor":"#2284e2",
+	        "textColor":"#000000"
+	      }
+	    }
+	  }
+	};
+	myCalendar.addStyles(newMonthLabelColorStyle);
+	
 	myDialog.layout.addChild(myCalendar);
-	myDialog.layout.alpha = 0.1;
+    
+    var buttonSelect = new Button();
+    buttonSelect.height = 60;
+    buttonSelect.width = Screen.width - margin*2;;
+    buttonSelect.left = margin;
+    buttonSelect.text = lang["pgNewLeaveRequest.selectDate"];
+    buttonSelect.top = top + height - 10;
+    buttonSelect.backgroundColor = Color.WHITE;
+    buttonSelect.textColor = Color.create(23,117,208);
+    buttonSelect.onPress = function(){
+    	if (selectedDate) {
+    		self.selectDatePicker.pickerText.text = selectedDate;
+    	}else{
+    		self.selectDatePicker.pickerText.text = "";
+    	}
+    	myDialog.hide();
+    }
+    myDialog.layout.addChild(buttonSelect);
+    
+    var buttonCancel= new Button();
+    buttonCancel.height = 60;
+    buttonCancel.width = Screen.width - margin*2;;
+    buttonCancel.left = margin;
+    buttonCancel.text = lang["cancel"];
+    buttonCancel.top = top + height + - 5;
+    buttonCancel.backgroundColor = Color.create(23,117,208);
+    buttonCancel.onPress = function(){
+    	myDialog.hide();
+    };
+    buttonCancel.textColor = Color.WHITE;
+    myDialog.layout.addChild(buttonCancel);
+    
     myDialog.layout.applyLayout();
-    myCalendar.changeCalendar("en","gregorian",{});
+    
 	this.flexDate.onTouchEnded = function(){
 		myDialog.show();
+	};
+	
+	this.flexType.onTouchEnded = function(){
+		myPicker.show(okCallback, cancelCallback);
 	};
 }
 
